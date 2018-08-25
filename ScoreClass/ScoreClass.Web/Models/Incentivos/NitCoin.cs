3a8e1af444fa19f5052a40a9457ecd409825eaf7
Bruno N. Fernandes <provider.bnogueira@terceiros.mongeralaegon.com.br>
2018-08-25 14:19:47 -0300

    Separando classes em arquivos

--- ScoreClass/ScoreClass.Web/Models/Incentivos/NitCoin.cs
+++ ScoreClass/ScoreClass.Web/Models/Incentivos/NitCoin.cs
@@ -1,8 +1,5 @@
 ﻿using ScoreClass.Web.Models.Cadastros;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ScoreClass.Web.Models.Incentivos
 {
@@ -11,35 +8,7 @@ namespace ScoreClass.Web.Models.Incentivos
 		public Int64 Id { get; set; }
 		public Responsavel Responsavel { get; set; }
 		public Int32 Quantidade { get; set; }
+		public DateTime Registro { get; set; }
 		public String Descricao { get; set; }
 	}
-
-	public class Voucher
-	{
-		public Int64 Id { get; set; }
-		public NitCoin NitCoinOrigem { get; set; }
-		public Fidelidade Fidelidade { get; set; }
-		public Int32 Quantidade { get; set; }
-		public Int32 Valor { get; set; }
-		public String Codigo { get; set; }
-		public DateTime Validade { get; set; }
-		public Boolean Resgatado { get; set; }
-	}
-
-	public class Fidelidade
-	{
-		public Int64 Id { get; set; }
-		public Parceria Parceria { get; set; }
-		public DateTime InicioVigencia { get; set; }
-		public DateTime FimVigencia { get; set; }
-		public Decimal TaxaConversao { get; set; }
-		public Int32 TempoVigenciaEmDias { get; set; }
-	}
-
-	public class Parceria
-	{
-		public Int64 Id { get; set; }
-		public List<Fidelidade> Programas { get; set; }
-	}
-
 }
