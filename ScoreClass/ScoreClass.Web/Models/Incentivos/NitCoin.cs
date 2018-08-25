7a39dc5a5daa5c759f0da36c51d7253f7280309d
Bruno N. Fernandes <provider.bnogueira@terceiros.mongeralaegon.com.br>
2018-08-25 13:18:25 -0300

    Subindo testes

---
+++ ScoreClass/ScoreClass.Web/Models/Incentivos/NitCoin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreClass.Web.Models.Incentivos
+{
+    public class NitCoin
+    {
+		public Int64 Id { get; set; }
+		public Int32 Quantidade { get; set; }
+		public String Descricao { get; set; }
+	}
+
+	public class Voucher
+	{
+
+	}
+
+}
