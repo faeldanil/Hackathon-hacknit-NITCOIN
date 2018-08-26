0965a6ab96f9e5805dd210447c5543a2cca4a3cb
Bruno N. Fernandes <provider.bnogueira@terceiros.mongeralaegon.com.br>
2018-08-26 03:49:28 -0300

    Setup de Ocorrencias

--- ScoreClass/ScoreClass.Web/Models/Cadastros/Ocorrencia.cs
+++ ScoreClass/ScoreClass.Web/Models/Cadastros/Ocorrencia.cs
@@ -4,7 +4,8 @@ namespace ScoreClass.Web.Models.Cadastros
 {
 	public class Ocorrencia : Entidade
 	{
-        public DateTime DataHora { get; set; }
+		public Materia Materia { get; set; }
+		public DateTime DataHora { get; set; }
         public string Descricao { get; set; }
         public StatusAvaliacao TipoComportamento { get; set; }
     }
