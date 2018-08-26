38cce96a85e508f584bd8ecb200538960b23bf2f
Bruno N. Fernandes <provider.bnogueira@terceiros.mongeralaegon.com.br>
2018-08-26 06:04:24 -0300

    Parceria e Fidelidade

---
+++ ScoreClass/ScoreClass.Web/Controllers/ParceriaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScoreClass.Web.Controllers
+{
+	public class ParceriaController : BaseController
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Fidelidade()
+		{
+			return View();
+		}
+	}
+}
