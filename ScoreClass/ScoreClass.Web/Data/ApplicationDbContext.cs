766ff58ffcee977f7f9fbb830a780eb89dd0d2b7
fornec <eduardoclark@gmail.com>
2018-08-25 22:03:04 -0300

    matriculas e materias adicionais no seed

--- ScoreClass/ScoreClass.Web/Data/ApplicationDbContext.cs
+++ ScoreClass/ScoreClass.Web/Data/ApplicationDbContext.cs
@@ -10,60 +10,93 @@ using ScoreClass.Web.Models.Incentivos;
 
 namespace ScoreClass.Web.Data
 {
-	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
-		{
-			Seed();
-		}
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Seed();
+        }
 
-		private void Seed()
-		{
-			if (!Responsavel.Any())
-			{
-				var escola = new Escola { Nome = "ScoreClass", Turmas = new List<Turma>() };
-				var turma = new Turma { Nome = "7º Ano", Escola = escola};
-				escola.Turmas.Add(turma);
-				
-				var responsavel = new Responsavel() { Nome = "Glautter", Email = new Email { Descricao = "glautterg@gmail.com" } };
-				var aluno1 = new Aluno { Nome = "Aluno 1", Reponsavel = responsavel, Email = new Email { Descricao = "aluno1@nitcoin.com.br" } };
-				var aluno2 = new Aluno { Nome = "Aluno 2", Reponsavel = responsavel, Email = new Email { Descricao = "aluno2@nitcoin.com.br" } };
+        private void Seed()
+        {
+            if (!Responsavel.Any())
+            {
+                var escola = new Escola { Nome = "ScoreClass", Turmas = new List<Turma>() };
+                var turma = new Turma { Nome = "7º Ano", Escola = escola };
+                escola.Turmas.Add(turma);
 
-				var matricula1 = new Matricula { Aluno = aluno1, Turma = turma };
-				var matricula2 = new Matricula { Aluno = aluno2, Turma = turma };
+                var responsavel = new Responsavel() { Nome = "Glautter", Email = new Email { Descricao = "glautterg@gmail.com" } };
+                var aluno1 = new Aluno { Nome = "Aluno 1", Reponsavel = responsavel, Email = new Email { Descricao = "aluno1@nitcoin.com.br" } };
+                var aluno2 = new Aluno { Nome = "Aluno 2", Reponsavel = responsavel, Email = new Email { Descricao = "aluno2@nitcoin.com.br" } };
 
-				aluno1.Matriculas.Add(matricula1);
-				aluno2.Matriculas.Add(matricula1);
+                var matricula1 = new Matricula { Aluno = aluno1, Turma = turma, Codigo = "codigo 1" };
+                var matricula2 = new Matricula { Aluno = aluno2, Turma = turma, Codigo = "codigo 2" };
 
-				turma.Matriculas.Add(matricula1);
-				turma.Matriculas.Add(matricula2);
+                var materia1a1 = new Materia { Aluno = aluno1, Nome = "matematica" };
+                var materia2a1 = new Materia { Aluno = aluno1, Nome = "portugues" };
+                var materia3a1 = new Materia { Aluno = aluno1, Nome = "ciencias sociais" };
+                var materia4a1 = new Materia { Aluno = aluno1, Nome = "historia" };
+                var materia5a1 = new Materia { Aluno = aluno1, Nome = "geografia" };
+                var materia6a1 = new Materia { Aluno = aluno1, Nome = "educacao fisica" };
+                
+                var materia1a2 = new Materia { Aluno = aluno2, Nome = "matematica" };
+                var materia2a2 = new Materia { Aluno = aluno2, Nome = "portugues" };
+                var materia3a2 = new Materia { Aluno = aluno2, Nome = "ciencias sociais" };
+                var materia4a2 = new Materia { Aluno = aluno2, Nome = "historia" };
+                var materia5a2 = new Materia { Aluno = aluno2, Nome = "geografia" };
+                var materia6a2 = new Materia { Aluno = aluno2, Nome = "educacao fisica" };
 
-				Responsavel.Add(responsavel);
-				Aluno.Add(aluno1);
-				Aluno.Add(aluno2);
-				Escola.Add(escola);
-				SaveChanges();
-			}
-		}
+                matricula1.Materias.Add(materia1a1);
+                matricula1.Materias.Add(materia2a1);
+                matricula1.Materias.Add(materia3a1);
+                matricula1.Materias.Add(materia4a1);
+                matricula1.Materias.Add(materia5a1);
+                matricula1.Materias.Add(materia6a1);
 
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
-			base.OnModelCreating(builder);
-		}
+                matricula2.Materias.Add(materia1a2);
+                matricula2.Materias.Add(materia2a2);
+                matricula2.Materias.Add(materia3a2);
+                matricula2.Materias.Add(materia4a2);
+                matricula2.Materias.Add(materia5a2);
+                matricula2.Materias.Add(materia6a2);
 
-		public virtual DbSet<Aluno> Aluno { get; set; }
-		public virtual DbSet<Escola> Escola { get; set; }
-		public virtual DbSet<Matricula> Matricula { get; set; }
-		public virtual DbSet<Materia> Materia { get; set; }
-		public virtual DbSet<Responsavel> Responsavel { get; set; }
-		public virtual DbSet<Pontualidade> Pontualidade { get; set; }
-		public virtual DbSet<Nota> Nota { get; set; }
-		public virtual DbSet<NitCoin> NitCoin { get; set; }
-		public virtual DbSet<Ocorrencia> Ocorrencia { get; set; }
-		public virtual DbSet<Turma> Turma { get; set; }
-		public virtual DbSet<Frequencia> Frequencia { get; set; }
-		public virtual DbSet<Professor> Professor { get; set; }
-		public virtual DbSet<Voucher> Voucher { get; set; }
-	}
+                materia1a1.Notas.Add(new Nota {Descricao= "1o", Materia= materia1a1});
+
+
+
+                aluno1.Matriculas.Add(matricula1);
+                aluno2.Matriculas.Add(matricula1);
+
+
+                turma.Matriculas.Add(matricula1);
+                turma.Matriculas.Add(matricula2);
+
+                Responsavel.Add(responsavel);
+                Aluno.Add(aluno1);
+                Aluno.Add(aluno2);
+                Escola.Add(escola);
+                SaveChanges();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public virtual DbSet<Aluno> Aluno { get; set; }
+        public virtual DbSet<Escola> Escola { get; set; }
+        public virtual DbSet<Matricula> Matricula { get; set; }
+        public virtual DbSet<Materia> Materia { get; set; }
+        public virtual DbSet<Responsavel> Responsavel { get; set; }
+        public virtual DbSet<Pontualidade> Pontualidade { get; set; }
+        public virtual DbSet<Nota> Nota { get; set; }
+        public virtual DbSet<NitCoin> NitCoin { get; set; }
+        public virtual DbSet<Ocorrencia> Ocorrencia { get; set; }
+        public virtual DbSet<Turma> Turma { get; set; }
+        public virtual DbSet<Frequencia> Frequencia { get; set; }
+        public virtual DbSet<Professor> Professor { get; set; }
+        public virtual DbSet<Voucher> Voucher { get; set; }
+    }
 }
